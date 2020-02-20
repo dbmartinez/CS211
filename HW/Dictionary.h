74a55114e88821e8b20f841745afac626432d536
Daniel Martinez <57377837+dbmartinez@users.noreply.github.com>
2020-02-19 16:21:07 -0800

    Add files via upload

---
+++ HW/Dictionary.h
@@ -0,0 +1,46 @@
+// Daniel Martinez
+// marti931@cougars.csusm.edu
+// Assignment 2
+// CS 211
+// Professor: Dr. Ahmad R. Hadaegh
+// Due Date: October 31, 2019   
+
+/**************************************
+* The Dictionary.h file will hold all *
+* the data members. Dictionary.cpp    *
+* will have access to these data      *
+* members and their attributes.       *
+**************************************/
+// Date Written: 10/24/2019
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Dictionary
+{
+ private:
+  const int maxWordsInDict;
+  const int maxWordsPerFile;
+  int totalWordsInDict;
+  int numOfWordsInFile[26];
+  static bool failure;
+  static bool success;
+  
+ public:
+  Dictionary();
+  ~Dictionary();
+  Dictionary(int dictMaxWords, int fileMaxWords);
+  bool AddAWord(string word);
+  bool DeleteAWord(string word);
+  bool SearchForWord(string word);
+  bool PrintAFileInDict(string filename);
+  bool SpellChecking(string fileName);
+  bool InsertWordsIntoDict(string fileName);
+  void ProcessTransactionFile();
+  string ToLower(string word);
+  string ToUpper(string word);
+};
+#endif
