454e74f9309523f9380bf4994330de248052a018
Daniel Martinez <57377837+dbmartinez@users.noreply.github.com>
2020-02-19 16:26:51 -0800

    Add files via upload

---
+++ HW/A2/A2-Martinez,Daniel.cpp
@@ -0,0 +1,32 @@
+// Daniel Martinez
+// marti931@cougars.csusm.edu
+// Assignment 2
+// CS 211
+// Professor: Dr. Ahmad R. Hadaegh
+// Due Date: October 31, 2019 
+
+
+/***********************************************
+* Write an object-oriented program to create a *
+* dictionary.                                  *
+*                                              *
+* Input: The user will input a Transaction.txt *
+* file with a list of commands and data.       *
+*                                              *
+* Output: Will display text files from S.txt,  *
+* C.txt, H.txt, T.txt                          *
+***********************************************/
+// Date Written: 10/24/2019
+#include "Dictionary.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  Dictionary DictForCS211;
+  DictForCS211.ProcessTransactionFile();
+  return 0;
+}
